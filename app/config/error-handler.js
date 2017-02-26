fb15060df3e4463d5b12b76f2cb23036d1a28c50
sagar <sagar_kharel84@yahoo.com>
2017-02-26 17:15:08 -0500

    fake a response

--- app/config/error-handler.js
+++ app/config/error-handler.js
@@ -4,8 +4,7 @@ import * as logger from './logger';
 
 let env = process.env.NODE_ENV;
 
-let exp = {},
-    expressApp;
+let exp = {};
 
 // catch 404 and forward to error handler
 let handle404 = function (req, res, next) {
@@ -17,30 +16,19 @@ let handle404 = function (req, res, next) {
 
 //error handler. Stack trace is set only in development
 let handleUnexpectedErrors = function (err, req, res) {
-    console.log('--> Handling unexpected errors');
+
+    logger.error(err);
     let error = {
         status: err.status || 500,
         message: err.message || 'Unexpected error occurred',
-        err: err.status === 404 ? {}: err
     };
-    // error.status = err.status || 500;
-    // error.description = err.description || (err.status === 404 ? 'Not found' :'Unexpected error occurred');
-    // error.message = err.message;
-    // error.err = err;
 
-    logger.error(error);
-
-    //we logged everything! Now update here not to leak strack trace in production
-    error.err = (/local/i.test(env) || /development/i.test(env)) ? err : {};
-
-    res.status(error.status).json(error);
+    res.status(error.status).end(error.message);
 };
 
 
-exp.initErrorHandler = (app) => {
+export let initErrorHandler = (app) => {
     app.use(handle404);
     app.use(handleUnexpectedErrors);
 };
-
-module.exports = exp;
 
--- app/model/person.js
+++ app/model/person.js
@@ -1,35 +1,3 @@
 'use strict';
 
-let mongoose = require('mongoose'),
-    validate = require('mongoose-validator'),
-    Schema = mongoose.Schema,
-    errorBuilder = require('../config/errorbuilder');
-
-
-let emailValidator = [
-        validate({
-            validator: 'isEmail',
-            message: 'not a valid email'
-        })];
-
-let Person = new Schema({
-    fname: {type: String, required: '{PATH} is required'},
-    lname: {type: String, required: '{PATH} is required'},
-    email: {type: String, required: '{PATH} is required', validate: emailValidator, index: {unique: true}}
-}, {minimize: false, autoIndex: false, validateBeforeSave: false, timestamps: {}});
-
-let personEmailRegex = (email) => new RegExp('^' + email + '$', 'i');
-
-Person.statics.findByEmail = (email, fields, successFn, errorFn) => {
-    fields = fields || 'fname lname email';
-    let emailRegex = personEmailRegex(email);
-    mongoose.models['Person'].find({email: {$regex: emailRegex}}, fields)
-        .then((success) => {
-            let profile = success[0] || {};
-            successFn(profile);
-        }, (err) => {
-            errorFn(err);
-        });
-};
-
-module.exports = mongoose.model('Person', Person);
+export let registerUser = () => Promise.reject('Testing Error response');
